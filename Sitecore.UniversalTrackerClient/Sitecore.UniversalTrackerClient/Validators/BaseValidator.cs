aa1b4e7e732a98f6354c408c2a6b72432b255a16
IGK <igk@sitecore.net>
2018-05-17 17:09:44 +0300

    code style fix

--- Sitecore.UniversalTrackerClient/Sitecore.UniversalTrackerClient/Validators/BaseValidator.cs
+++ Sitecore.UniversalTrackerClient/Sitecore.UniversalTrackerClient/Validators/BaseValidator.cs
@@ -1,56 +1,64 @@
 ﻿namespace Sitecore.UniversalTrackerClient.Validators
 {
-  using System;
+    using System;
 
-  public class BaseValidator
-  {
-    public static void CheckNullAndThrow(Object obj, string source)
+    public class BaseValidator
     {
-      if (obj == null)
-      {
-        throw new ArgumentNullException(source);
-      }
+        public static void CheckNullAndThrow(Object obj, string source)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(source);
+            }
+        }
+
+        public static void CheckForTwiceSetAndThrow(Object obj, string source)
+        {
+            if (obj != null)
+            {
+                throw new InvalidOperationException(source + " : " + "Property cannot be assigned twice.");
+            }
+        }
+
+        public static void CheckForTwiceSetAndThrow(int number, string source)
+        {
+            if (number >= 0)
+            {
+                throw new InvalidOperationException(source + " : " + "Property cannot be assigned twice.");
+            }
+        }
+
+        public static void AssertPositiveNumber(int? optionalNumber, string source)
+        {
+            BaseValidator.CheckNullAndThrow(optionalNumber, source);
+            int number = optionalNumber.Value;
+
+            if (number <= 0)
+            {
+                throw new ArgumentException(source + " : " + "Positive number expected");
+            }
+
+        }
+
+        public static void CheckForNullAndEmptyOrThrow(string str, string source)
+        {
+            CheckNullAndThrow(str, source);
+
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new ArgumentException(source + " : " + "The input cannot be empty.");
+            }
+        }
+
+        public static void CheckForNullEmptyAndWhiteSpaceOrThrow(string str, string source)
+        {
+            CheckNullAndThrow(str, source);
+
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                throw new ArgumentException(source + " : " + "The input cannot be empty.");
+            }
+        }
+
     }
-
-    public static void CheckForTwiceSetAndThrow(Object obj, string source)
-    {
-      if (obj != null)
-      {
-        throw new InvalidOperationException(source + " : " + "Property cannot be assigned twice.");
-      }
-    }
-
-    public static void AssertPositiveNumber(int? optionalNumber, string source)
-    {
-      BaseValidator.CheckNullAndThrow(optionalNumber, source);
-      int number = optionalNumber.Value;
-
-      if (number <= 0)
-      {
-        throw new ArgumentException(source + " : " + "Positive number expected");
-      }
-
-    }
-
-    public static void CheckForNullAndEmptyOrThrow(string str, string source)
-    {
-      CheckNullAndThrow(str, source);
-
-      if (string.IsNullOrEmpty(str))
-      {
-        throw new ArgumentException(source + " : " + "The input cannot be empty.");
-      }
-    }
-
-    public static void CheckForNullEmptyAndWhiteSpaceOrThrow(string str, string source)
-    {
-      CheckNullAndThrow(str, source);
-
-      if (string.IsNullOrWhiteSpace(str))
-      {
-        throw new ArgumentException(source + " : " + "The input cannot be empty.");
-      }
-    }
-
-  }
 }
