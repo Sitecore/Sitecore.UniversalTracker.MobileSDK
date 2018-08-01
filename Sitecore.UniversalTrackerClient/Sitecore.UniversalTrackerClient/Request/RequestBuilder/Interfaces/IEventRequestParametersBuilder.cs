024175b1a0e22bcbb9f2e677e08b230008a55986
IGK <igk@sitecore.net>
2018-08-01 17:56:25 +0300

    request builder interfaces moved to the separate folder

---
+++ Sitecore.UniversalTrackerClient/Sitecore.UniversalTrackerClient/Request/RequestBuilder/Interfaces/IEventRequestParametersBuilder.cs
@@ -0,0 +1,43 @@
+﻿namespace Sitecore.UniversalTrackerClient.Request.RequestBuilder
+{
+	using System;
+	using System.Collections.Generic;
+
+	public interface IEventRequestParametersBuilder<T> : IBaseRequestParametersBuilder<T>
+     where T : class
+    {
+
+		IEventRequestParametersBuilder<T> AddCustomValues(IDictionary<string, string> customValuesByName);
+
+        /// <summary>
+        /// Adds fields that will be updated in item.
+        /// </summary>
+        /// <param name="fieldName">Field name.</param>
+        /// <param name="fieldValue">Field raw value.</param>
+        /// <returns>
+        /// this
+        /// </returns>
+		IEventRequestParametersBuilder<T> AddCustomValues(string fieldName, string fieldValue);
+
+		IEventRequestParametersBuilder<T> DefinitionId(string definitionId);
+
+		IEventRequestParametersBuilder<T> ItemId(string itemId);
+
+		IEventRequestParametersBuilder<T> EngagementValue(int engagementValue);
+
+		IEventRequestParametersBuilder<T> ParentEventId(string parentEventId);
+
+		IEventRequestParametersBuilder<T> Text(string text);
+
+		IEventRequestParametersBuilder<T> Timestamp(DateTime timestamp);
+
+		IEventRequestParametersBuilder<T> Duration(TimeSpan duration);
+
+        /// <summary>
+        /// Optional device identifier, expected value - identifierForVendor for iOS, getSerial for android or similar...
+        /// </summary>
+        /// <returns>request</returns>
+        IEventRequestParametersBuilder<T> DeviceIdentifier(string deviceIdentifier);
+
+    }
+}
---
+++ Sitecore.UniversalTrackerClient/Sitecore.UniversalTrackerClient/Request/RequestBuilder/Interfaces/IPageViewRequestParametersBuilder.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Sitecore.UniversalTrackerClient.Request.RequestBuilder
+{
+    using System;
+    using System.Collections.Generic;
+    using Sitecore.UniversalTrackerClient.Entities;
+    using Sitecore.UniversalTrackerClient.UserRequest;
+
+    public interface IPageViewRequestParametersBuilder<T> : IEventRequestParametersBuilder<T>
+        where T : class
+    {
+        IPageViewRequestParametersBuilder<T> ItemLanguage(string itemLanguage);
+
+        IPageViewRequestParametersBuilder<T> ItemVersion(int itemVersion);
+
+        IPageViewRequestParametersBuilder<T> Url(string url);
+
+        IPageViewRequestParametersBuilder<T> SitecoreRenderingDevice(string id, string name);
+
+        #region fluent support
+
+        new IPageViewRequestParametersBuilder<T> AddCustomValues(IDictionary<string, string> customValues);
+
+        new IPageViewRequestParametersBuilder<T> AddCustomValues(string customFieldName, string customFieldValue);
+
+        new IPageViewRequestParametersBuilder<T> DefinitionId(string definitionId);
+
+        new IPageViewRequestParametersBuilder<T> ItemId(string itemId);
+
+        new IPageViewRequestParametersBuilder<T> EngagementValue(int engagementValue);
+
+        new IPageViewRequestParametersBuilder<T> ParentEventId(string parentEventId);
+
+        new IPageViewRequestParametersBuilder<T> Text(string text);
+
+        new IPageViewRequestParametersBuilder<T> Timestamp(DateTime timestamp);
+
+        new IPageViewRequestParametersBuilder<T> Duration(TimeSpan duration);
+
+        #endregion fluent support
+    }
+}
