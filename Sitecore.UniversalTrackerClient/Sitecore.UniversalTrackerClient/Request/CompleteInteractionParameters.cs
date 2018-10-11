e1f4f9945fcbdd5f64b0a16b301da2181a2bdd45
IGK <igk@sitecore.net>
2018-10-11 15:25:51 +0300

    interaction complition request added

---
+++ Sitecore.UniversalTrackerClient/Sitecore.UniversalTrackerClient/Request/CompleteInteractionParameters.cs
@@ -0,0 +1,36 @@
+﻿namespace Sitecore.UniversalTrackerClient.UserRequest
+{
+	using Sitecore.UniversalTrackerClient.Entities;
+	using Sitecore.UniversalTrackerClient.Session.Config;
+
+    public class CompleteInteractionParameters : ICompleteInteractionRequest
+    {
+        public CompleteInteractionParameters(IUTSessionConfig sessionConfig)
+        {
+            this.SessionConfig = sessionConfig.SessionConfigShallowCopy();
+            this.InteractionId = sessionConfig.ActiveInteractionId;
+        }
+
+        public virtual ICompleteInteractionRequest DeepCopyCompleteInteractionRequest()
+        {
+            IUTSessionConfig connection = null;
+
+            if (null != this.SessionConfig)
+            {
+                connection = this.SessionConfig.SessionConfigShallowCopy();
+            }
+
+            return new CompleteInteractionParameters(connection);
+        }
+
+		public IBaseRequest DeepCopyBaseRequest()
+		{
+            return this.DeepCopyCompleteInteractionRequest();
+		}
+      
+        public IUTSessionConfig SessionConfig { get; private set; }
+        public string InteractionId { get; private set; }
+
+    }
+}
+
